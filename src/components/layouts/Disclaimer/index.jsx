e8cbb5b91e014a3dc3fe79376ce1b0b5346583d5
Guillermo Gahbler <mexamericano@gmail.com>
2018-06-25 17:57:10 -0700

    udated

--- src/components/layouts/Disclaimer/index.jsx
+++ src/components/layouts/Disclaimer/index.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const Disclaimer = () =>{
   return(
-  <div>
-  <h1>Diclaimer page</h1>
+    <div className="App">
+    <header className="App-header">
+    
+                     </header>
+    <p className="App-intro">
+    
+    <h1>DISCLAIMER</h1>
+    <h2>
+    The ugly but important stuff 
+    </h2>
+    
+    </p>
+    <div className='disclaimer'>
+    <p> Before using this or any other fitness app you should consult your physician or other health care professional to determine if it is right for your needs. This site is designed as a tool only, do not rely on this information as a substitute, nor should it replace professional medical advice, diagnosis, or treatment by proceeding you do so at your own risk. With that out of the way let's get started with Fit Me (you) Up!
+    </p>
+    </div>
+    <div>
+    
+    <footer> 
+    <img className="img-giphy" src="https://media.giphy.com/media/3o85xunRezGKPOkcG4/giphy.gif" alt="logo"/>
+    <div className="theButtons">
+    
+            <button className="RegisterBtn">Fit Me Up now!</button>
+    </div>
+    </footer>  
+
+  </div>
   </div>
   )
 }
--- src/components/layouts/Home/index.jsx
+++ src/components/layouts/Home/index.jsx
@@ -11,7 +11,9 @@ const Home = () => {
           Welcome to Fit Me Up!
         </h1>
         </p>
-        <p> Whether you want to lose weight, gain weight, or just maintain current weight we got you!
+        <p> Your nutritional intake and exercise spotter to help you achieve your goals for losing, gaining, or just maintaining weight. 
+        <br/>
+        Which ever you chose we got you!
         </p>
         <div>
         {/* <img className="img-responsive" src="https://media.giphy.com/media/3o85xunRezGKPOkcG4/giphy.gif" alt="logo"/> */}
