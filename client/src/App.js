927a50d030a4701444ce00ab6acab0741af28965
Jan Balangue <jantonio.balangue@gmail.com>
2018-06-30 21:07:34 -0700

    Add WeeklyCharts column, bar chart and pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import {Home, Options, Disclaimer, NotFound} from './components/layouts'
-import {UserForm} from './components/UserForm'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home, Options, Disclaimer, NotFound } from './components/layouts'
+import { UserForm } from './components/UserForm'
 import './App.css';
+import { WeeklyCharts } from './components/WeeklyCharts';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path = "/" component ={Home}/>
-          <Route path = "/disclaimer" component ={Disclaimer}/>
-          <Route path = "/options" component ={Options}/>
-          <Route component ={NotFound}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/disclaimer" component={Disclaimer} />
+          <Route path="/options" component={Options} />
+          <Route component={NotFound} />
           <Route component={UserForm} />
-        </Switch>  
+          <Route component={WeeklyCharts} />
+        </Switch>
       </BrowserRouter>
-      
+
 
     );
   }
---
+++ client/src/components/WeeklyCharts/index.js
@@ -0,0 +1,1 @@
+export {default as WeeklyCharts} from "./WeeklyCharts";
