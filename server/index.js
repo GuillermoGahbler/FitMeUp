1514d0b7b2af38e60710641afb59532e3c03778b
Jan Balangue <jantonio.balangue@gmail.com>
2018-07-05 20:48:40 -0700

    Modify various files. Now writes tables to Sequelize database

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3001;
 const authRouter = require('./routes/authRoutes');
-const authorization = require ('./authorization');
+const authorization = require('./authorization');
 const db = require('./models/index');
 
 server.use(authorization.initialize());
 server.use(authorization.session());
 server.use("/auth", authRouter);
 
-server.listen(PORT,()=>{
-  console.log(`listening on http://localhost${PORT}`)
-})
+db.sequelize.sync({ force: true }).then(function () {
+  console.log('Database looks ok');
+  server.listen(PORT, () => {
+    console.log(`Site is live. Listening on http://localhost:${PORT}`);
+  });
+}).catch(function (err) {
+  console.log(err, "Something went wrong with the Database Update!");
+});
+
+// server.listen(PORT,()=>{
+//   console.log(`listening on http://localhost${PORT}`)
+// })
 
