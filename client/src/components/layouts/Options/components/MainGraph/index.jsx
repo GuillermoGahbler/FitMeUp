41cee5d0bace724d491d7bb1abd25637dd7258cd
Hnin Haymar <hninhaymar@gmail.com>
2018-07-19 02:56:06 -0700

    Made the main graph populate with dynamic data

--- client/src/components/layouts/Options/components/MainGraph/index.jsx
+++ client/src/components/layouts/Options/components/MainGraph/index.jsx
@@ -5,12 +5,6 @@ import { Chart } from "chart.js"
 
 class IntakeChart extends React.Component {
 
-  constructor(props){
-    super(props);
-    console.log("main graph props");
-    console.log(props);    
-  }
-
   state = {
     weeks: [],
     weight: [],
@@ -22,77 +16,68 @@ class IntakeChart extends React.Component {
 
 
 
-  readWeeks = (id)=>{
+  readWeeks = (id,chartId)=>{
     axios.get(`/weeks/${id}`)
     .then(res=>{
+
       this.setState({
         weeks : res.data,
         weight: res.data.reduce((acc, week) => [...acc, week.weight], []),
         bodyfat : res.data.reduce((acc, week) => [...acc, week.bodyFat], []),
         leanbodymass : res.data.reduce((acc, week) => [...acc, week.bodyMass], []),
         fatmass : res.data.reduce((acc, week) => [...acc, week.fatMass], []),
-        labels : Array(res.data.reduce((acc, week) => [...acc, week.weight], []).length).map((ele,index) => `week${index + 1}`)
+        labels : res.data.map((ele,index) => `week${index + 1}`)
       });
-
-      console.log("MainGraph state");
-      console.log(this.state);
-
-     /* weightArray = this.state.weeks.reduce((acc, week) => [...acc, week.weight], []);
-    bfArray = this.state.weeks.reduce((acc, week) => [...acc, week.bodyFat], []);
-    lbmArray = this.state.weeks.reduce((acc, week) => [...acc, week.bodyMass], []);
-    fmArray = this.state.weeks.reduce((acc, week) => [...acc, week.fatMass], []);
-    labelsArray = Array(this.weightArray.length).map((ele, index) => `week${index + 1}`);*/
+      
+      new Chart(chartId, {
+        type: 'line',
+        data: {
+          labels: this.state.labels,
+          datasets: [{
+            label: 'Weight',
+            data: this.state.weight,
+            backgroundColor: [
+              'rgba(100, 100, 100, 0.2)',
+  
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+  
+            ],
+            borderWidth: 1
+          },
+          {
+            label: 'Body Fat',
+            data: this.state.fatmass,
+            backgroundColor: [
+              'rgba(100, 100, 100, 0.2)',
+  
+            ],
+            borderColor: [
+              'rgba(155, 1, 32, 1)',
+  
+            ],
+            borderWidth: 1
+          }],
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          }
+        }
+      });
     })
   }
 
   componentDidMount() {
     let ctx = document.getElementById('myChart').getContext('2d');
-    console.log("User ID");
-    console.log(this.props.account_id);
 
-    this.readWeeks(this.props.account_id);
+    this.readWeeks(this.props.account_id,ctx);
 
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: this.state.labels,
-        datasets: [{
-          label: 'Weight',
-          data: this.state.weight,
-          backgroundColor: [
-            'rgba(100, 100, 100, 0.2)',
-
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-
-          ],
-          borderWidth: 1
-        },
-        {
-          label: 'Body Fat',
-          data: this.state.fatmass,
-          backgroundColor: [
-            'rgba(100, 100, 100, 0.2)',
-
-          ],
-          borderColor: [
-            'rgba(155, 1, 32, 1)',
-
-          ],
-          borderWidth: 1
-        }],
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        }
-      }
-    });
   }
 
   render() {
